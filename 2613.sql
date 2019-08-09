8f02fc1a16775d16563fd8c169d9c31f84be952e
Victoria Vilas Boas <vick_vilasb@outlook.com>
2019-08-08 22:38:48 -0300

    problem 2613

--- 2613.sql
+++ 2613.sql
@@ -1,9 +1,4 @@
-select mov.id, mov.name from movies as mov
-inner join movies_actors as mv
-on mv.id_movies = mov.id
-inner join genres as gen 
-on gen.id = mov.id_genres
-inner join actors as ac
-on ac.id = mv.id_actors
-where ac.name like '%Silva%' and gen.description='Action'
-group by mov.id;
+select mo.id, mo.name from movies as mo
+inner join prices as pri
+on pri.id = mo.id_prices 
+where pri.value < 2;
